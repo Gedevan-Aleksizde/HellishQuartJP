ee62e717d1fda06df487fa9a50055921da35f34a
S-Katagiri <katagiri.stsh@gmail.com>
2024-03-20 22:24:09 +0900

    v1.2.1

--- make-installer.ps1
+++ make-installer.ps1
@@ -1,6 +1,6 @@
 Param($arch = "x64", $lang = "ja", [switch]$build)
 
-${VERSION} = "1.2.0-2023.12.15"
+${VERSION} = "1.2.1-2024.03.17.0"
 ${BuildDirML} = "MelonLoader/Output/Release"
 ${BuildDirXU} = "XUnity.AutoTranslator/dist"
 ${ConfDir} = "XUATConfig"
